2bd3e61ddc73ed057657d9dbf71b37e63377265f
Saikiran Katraj <saikirankatraj@gmail.com>
2026-01-09 17:20:25 +0530

    Update First.py

--- First.py
+++ First.py
@@ -1,4 +1,26 @@
-a = 1
-b = 2
+a = int(input(" Enter your the number: "))
+b = int(input(" Enter your the number: "))
+c = int(input(" Enter your the number: "))
+d = int(input(" Enter your the number: "))
 
-print( a + b )
+if(a>b and a>c and a>d):
+    print(" a is greater number")
+  
+elif(b>c):
+    print(" b is greater number")
+
+elif(c>d):
+    print(" c is greater number")
+
+elif(d>a):
+    print(" d is greater number")
+
+    
+else:
+    print("all are or equal")
+
+
+print("End of the programe")
+
+
+

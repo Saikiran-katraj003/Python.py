7bc6442b90b0745f0cb93b5f04d72340f06d84d9
Saikiran Katraj <saikirankatraj@gmail.com>
2026-01-09 19:35:39 +0530

    Update First.py

--- First.py
+++ First.py
@@ -1,26 +1,35 @@
-a = int(input(" Enter your the number: "))
-b = int(input(" Enter your the number: "))
-c = int(input(" Enter your the number: "))
-d = int(input(" Enter your the number: "))
+# a = int(input(" Enter your the number: "))
+# b = int(input(" Enter your the number: "))
+# c = int(input(" Enter your the number: "))
+# d = int(input(" Enter your the number: "))
 
-if(a>b and a>c and a>d):
-    print(" a is greater number")
+# if(a>b and a>c and a>d):
+#     print(" a is greater number")
   
-elif(b>c):
-    print(" b is greater number")
+# elif(b>c):
+#     print(" b is greater number")
 
-elif(c>d):
-    print(" c is greater number")
+# elif(c>d):
+#     print(" c is greater number")
 
-elif(d>a):
-    print(" d is greater number")
+# elif(d>a):
+#     print(" d is greater number")
 
     
-else:
-    print("all are or equal")
+# else:
+#     print("all are or equal")
 
 
-print("End of the programe")
+# print("End of the programe")
+
+
+a = int(input(" Enter the number:"))
+
+if(a%2==0):
+
+    print (" The number is even")
+else:  
+    print(" The number is odd")  
 
 
 

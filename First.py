8b02987f0a672bebdafb68bc2c6547784b0d2476
Saikiran Katraj <saikirankatraj@gmail.com>
2026-01-10 19:13:07 +0530

    Update First.py

--- First.py
+++ First.py
@@ -1,35 +1,47 @@
-# a = int(input(" Enter your the number: "))
-# b = int(input(" Enter your the number: "))
-# c = int(input(" Enter your the number: "))
-# d = int(input(" Enter your the number: "))
+import random
 
-# if(a>b and a>c and a>d):
-#     print(" a is greater number")
-  
-# elif(b>c):
-#     print(" b is greater number")
+'''
+1 for snake
+0 for gun
+-1 for water
+ '''
 
-# elif(c>d):
-#     print(" c is greater number")
+computer = random.choice([-1, 0, 1])
+youstr = input("Enter your choice: ")
+YouDict = {"snake": 1, "gun": 0, "water": -1}
+reverseDict= {1: "snake", 0: "gun", -1: "water"}
 
-# elif(d>a):
-#     print(" d is greater number")
+you = YouDict[youstr] 
+          
+          #by now we have 2 variables computer and you.
 
-    
-# else:
-#     print("all are or equal")
+print(f"You chose {reverseDict[you]} \n Computer chose {reverseDict[computer]}")
 
+if(computer == you):  
+    print("Its a draw")
 
-# print("End of the programe")
+else:
+    if(computer ==-1 and you == 1): #if computer and you chose same choice then it's a draw
+        print("You win!")
 
+    elif(computer ==-1 and you == 0): #if computer chose -1 and you chose 0 then you lose 
+        print("You Lose!")
 
-a = int(input(" Enter the number:"))
+    elif(computer == 1 and you == -1): #if computer chose 1 and you chose -1 then you lose 
+        print("You lose!")
 
-if(a%2==0):
+    elif(computer ==1 and you == 0): #if computer chose 1 and you chose 0 then you Win
+        print("You Win!")
 
-    print (" The number is even")
-else:  
-    print(" The number is odd")  
+    elif(computer ==0 and you == -1): #if computer chose 0 and you chose -1 then you Win 
+        print("You Win!")
+
+    elif(computer == 0 and you == 1): #if computer chose 0 and you chose 1 then you lose 
+        print("You Lose!")
+
+    else:
+        print("Something went wrong!") #if anything wrong in ifelse ladder then it will print Something went wrong
+
 
 
 
